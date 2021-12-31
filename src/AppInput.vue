d411c2f1a67bcc00216a1a0e158e82f6d79b201a
Denis <wolf-den@tut.by>
2021-12-31 11:12:04 +0300

    create custom element-modal for form

---
+++ src/AppInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form-control" :class="{invalid: error}">
+    <label :for="id">{{ label }}</label>
+    <input
+      type="text"
+      :id="id"
+      :placeholder="placeholder"
+      :value="value"
+    @input="onChange"
+    >
+    <!--      :value="modelValue"            default-->
+    <small v-if="error">{{ error }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  // emits: ['update:modelValue'],        default
+  emits: ['update:value'],
+  props: {
+    // modelValue: String,         default
+    value: String,
+    label: String,
+    placeholder: String,
+    error: String
+  },
+  methods: {
+    onChange(event) {
+      // this.$emit('update:modelValue', event.target.value)          default
+      this.$emit('update:value', event.target.value)
+    }
+  },
+  data() {
+    return {
+      id: 'input' + Math.random()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
